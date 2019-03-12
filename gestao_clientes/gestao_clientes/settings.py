235eb7b2d8c034682667c2120f54a68643ec6145
Allan Kovac <allan.costa@codebuddy.com..br>
2019-03-12 18:48:53 -0300

    Fazendo com que os arquivos de Midia funcione

--- gestao_clientes/gestao_clientes/settings.py
+++ gestao_clientes/gestao_clientes/settings.py
@@ -118,4 +118,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
+MEDIA_ROOR = 'media'
+MEDIA_URL = '/media/'
 STATIC_URL = '/static/'
+
--- gestao_clientes/gestao_clientes/urls.py
+++ gestao_clientes/gestao_clientes/urls.py
@@ -15,8 +15,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 from clientes import urls as clientes_urls
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('clientes/', include(clientes_urls))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
